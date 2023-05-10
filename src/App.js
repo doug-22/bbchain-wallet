eacc1f9c4770ad101386c534b3201dfd64969fa4
Douglas <douglas.aguiar410@gmail.com>
2023-05-10 10:48:22 -0300

    feat(home): looking for wallet balance

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
 import Home from './pages/Home'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 
 function App() {
   return (
-    <Home />
+    <>
+      <Home />
+      <ToastContainer
+        theme="light"
+        position="top-center"
+        autoClose={5000}
+        newestOnTop
+        bodyStyle={{ fontSize: '14px' }}
+      />
+    </>
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,42 @@
+import { useState } from 'react'
 import ImageMetaMask from '../../assets/MetaMask_Fox.png'
+import { hasEthereumInWindow, getMetaMaskAccounts } from '../../utils/functions'
+import { toast } from 'react-toastify'
 import './styles.css'
 
 const Home = () => {
+
+  const [showDownloadMetaMask, setShowDownloadMetaMask] = useState(false);
+  const [address, setAddress] = useState('');
+  const [balance, setBalance] = useState(0);
+
+  async function getBalance(address) {
+    const hasMetaMask = hasEthereumInWindow();
+    if (!hasMetaMask) {
+      setShowDownloadMetaMask(true);
+      return
+    }
+    const web3 = await getMetaMaskAccounts();
+    const balance = await web3.eth.getBalance(address);
+    return web3.utils.fromWei(balance);
+  }
+
+  async function conectMetaMask(event) {
+    event.preventDefault()
+    if (address) {
+      try {
+        await getBalance(address).then((response) => {
+          toast.success('Conexão realizada com sucesso!')
+          setBalance(response)
+        })
+      }
+      catch (error) {
+        throw new Error(error.message)
+      }
+    }
+  }
+
+
   return (
     <div className="background">
       <div className="wrapper">
@@ -9,18 +44,23 @@ const Home = () => {
           <h1 className="title">BBChain Wallet</h1>
           <img className='image-metamask' src={ImageMetaMask} alt="Logo MetaMask" />
         </div>
-        <div className='content'>
+        <form className='content' name='form' onSubmit={(event) => conectMetaMask(event)}>
           <div className='address'>
             <label>Endereço da Conta: </label>
-            <input type="text" id="account" placeholder='Insira o Endereço da Conta' />
+            <input type="text" id="account" placeholder='Insira o Endereço da Conta' required onChange={(event) => setAddress(event.target.value)} />
           </div>
           <div className='balance'>
             <p>Saldo Disponível:</p>
-            <p>10000</p>
+            <p>{balance}</p>
           </div>
-          <button className='conect-button'>Conectar</button>
-        </div>
+          <input className='conect-button' type='submit' value="Conectar" />
+        </form>
       </div>
+      {showDownloadMetaMask && (
+        <div className='wrapper-help'>
+          <p>Você não possui o MetaMask instalado no seu navegador, clique <a href="https://metamask.io/" target="_blank" rel="noreferrer">aqui</a> para ser redirecionando para a instalação!</p>
+        </div>
+      )}
     </div>
   )
 }
